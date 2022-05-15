56c816ffdabd735480002f78796c1b8ec865a8a3
Grant Findlay <grant@tate.it>
2022-05-15 21:25:16 +0100

    style activity section

--- components/activity/index.tsx
+++ components/activity/index.tsx
@@ -1,7 +1,14 @@
 import * as anchor from "@project-serum/anchor";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Skeleton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { useConnection } from "@solana/wallet-adapter-react";
-import { useMemo } from "react";
 import { useQuery } from "react-query";
 import * as utils from "../../utils";
 import { fetchParsedTransactions } from "../../common/query";
@@ -10,59 +17,131 @@ interface ActivityProps {
   mint?: anchor.web3.PublicKey;
 }
 
+interface Activity {
+  key: string;
+  type: "buy" | "listing";
+  blockTime: number | null | undefined;
+  lamports: anchor.BN;
+}
+
 export function Activity({ mint }: ActivityProps) {
   const { connection } = useConnection();
-  const transactionsQuery = useQuery(
-    ["transactions", mint?.toBase58()],
-    () => {
+
+  const activityQuery = useQuery(
+    ["activity", mint?.toBase58()],
+    async () => {
       if (mint) {
-        return fetchParsedTransactions(connection, mint);
+        const parsedTransactions = await fetchParsedTransactions(
+          connection,
+          mint
+        );
+
+        return parsedTransactions.map(mapTransaction).filter(Boolean);
       }
     },
     { enabled: Boolean(mint), refetchOnWindowFocus: false }
   );
 
-  const parsedActivity = useMemo(() => {
-    if (transactionsQuery.data) {
-      return transactionsQuery.data
-        .map((txn) => {
-          if (txn?.meta?.logMessages) {
-            const isBuyEvent = txn.meta.logMessages.some((log) =>
-              log.includes("Buy")
-            );
+  function renderActivityDetails(activity: Activity) {
+    switch (activity.type) {
+      case "buy":
+        return (
+          <Text>
+            Sold for{" "}
+            <Text as="span" fontWeight="semibold">
+              {utils.formatAmount(activity.lamports, 2)}
+            </Text>
+          </Text>
+        );
 
-            if (isBuyEvent) {
-              return {
-                key: txn.transaction.signatures[0],
-                blockTime: txn.blockTime,
-                lamports: new anchor.BN(txn.meta.preBalances[0]).sub(
-                  new anchor.BN(txn.meta.postBalances[0])
-                ),
-              };
-            }
-          }
+      case "listing":
+        return (
+          <Text>
+            Listed for{" "}
+            <Text as="span" fontWeight="semibold">
+              {utils.formatAmount(activity.lamports, 2)}
+            </Text>
+          </Text>
+        );
 
-          return null;
-        })
-        .filter(Boolean);
+      default:
+        return null;
     }
-  }, [transactionsQuery.data]);
+  }
 
   return (
-    <Box>
-      <Heading>Activity</Heading>
-      {parsedActivity?.map(
-        (activity) =>
-          activity && (
-            <Box key={activity.key}>
-              <Text>
-                {typeof activity.blockTime === "number" &&
-                  utils.formatBlockTime(activity.blockTime)}
-              </Text>
-              <Text>Sold for {utils.formatAmount(activity.lamports, 2)}</Text>
-            </Box>
-          )
+    <Box pt="6" pb="6">
+      <Heading size="md" mb="6">
+        Activity
+      </Heading>
+      <Divider mb="2" />
+      {activityQuery.isLoading ? (
+        <Stack>
+          <Skeleton h="8" />
+          <Skeleton h="8" />
+        </Stack>
+      ) : (
+        activityQuery.data?.map(
+          (activity) =>
+            activity && (
+              <Flex
+                key={activity.key}
+                justifyContent="space-between"
+                pt="4"
+                pb="4"
+              >
+                <Text color="gray.500" fontWeight="medium">
+                  {typeof activity.blockTime === "number" &&
+                    utils.formatBlockTime(activity.blockTime)}
+                </Text>
+                {renderActivityDetails(activity)}
+              </Flex>
+            )
+        )
       )}
     </Box>
   );
+}
+
+function mapTransaction(
+  txn: anchor.web3.ParsedTransactionWithMeta | null
+): Activity | null {
+  if (txn?.meta?.logMessages) {
+    const isBuyEvent = txn.meta.logMessages.some((log) => log.includes("Buy"));
+
+    if (isBuyEvent) {
+      return {
+        key: txn.transaction.signatures[0],
+        type: "buy",
+        blockTime: txn.blockTime,
+        lamports: new anchor.BN(txn.meta.preBalances[0]).sub(
+          new anchor.BN(txn.meta.postBalances[0])
+        ),
+      };
+    }
+
+    const isListing = txn.meta.logMessages.some((log) =>
+      log.includes("InitListing")
+    );
+
+    if (isListing) {
+      let data: null | string = null;
+
+      if ("data" in txn.transaction.message.instructions[0]) {
+        data = txn.transaction.message.instructions[0].data;
+        // TODO decode the listing price
+      }
+
+      return {
+        key: txn.transaction.signatures[0],
+        type: "listing",
+        blockTime: txn.blockTime,
+        lamports: new anchor.BN(txn.meta.preBalances[0]).sub(
+          new anchor.BN(txn.meta.postBalances[0])
+        ),
+      };
+    }
+  }
+
+  return null;
 }
