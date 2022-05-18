69659e2db50b6026e55e6b6edd019193e5328d95
Grant Findlay <dev@grantfindlay.com>
2022-05-18 11:05:08 +0100

    add logs and increase cache time

--- pages/api/floor/[symbol].ts
+++ pages/api/floor/[symbol].ts
@@ -10,7 +10,7 @@ type Data = {
 };
 
 const priceCache = new cache.Cache<string, number>();
-const CACHE_TIME = 30000;
+const CACHE_TIME = 1000 * 60 * 5; // 5 minutes
 
 export default async function handler(
   req: NextApiRequest,
@@ -26,10 +26,12 @@ export default async function handler(
   try {
     const name = utils.mapSymbolToCollectionName(symbol);
     const stats = await fetchMagicEdenCollectionStats(name);
+    console.log("stats", stats);
     priceCache.put(symbol, stats.floorPrice, CACHE_TIME);
 
     return res.status(200).json({ floorPrice: stats.floorPrice });
   } catch (error) {
+    console.log("error: ", error);
     const errorMessage =
       error instanceof Error ? error.message : "Internal Server Error";
 
