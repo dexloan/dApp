8cc9fccb112d0945687c58c928f9320aa1de3b22
Grant Findlay <dev@grantfindlay.com>
2022-10-04 12:11:03 +0100

    eslint-disable

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -68,6 +68,7 @@ export function Navbar() {
     if (wallet.publicKey) {
       signMessage();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [wallet.publicKey]);
 
   function UserMenuButton() {
